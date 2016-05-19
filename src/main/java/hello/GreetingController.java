b9cace912e5bee433427e89e5ea6cd13fcdc0b08
Shirish Gajul <shirishgajul@m-604g3qd.carfax.cfx>
2016-05-19 14:27:47 -0500

    Created first project onspring

--- src/main/java/hello/GreetingController.java
+++ src/main/java/hello/GreetingController.java
@@ -5,17 +5,15 @@ package hello;
  */
 
 
-import org.jboss.logging.Param;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import sun.awt.ModalExclude;
 
 @Controller
 public class GreetingController {
     @RequestMapping("/greeting")
-    public String greeting(@RequestParam (value= "name", required =false, defaultValue ="world") String name, Model model){
+    public String greeting(@RequestParam (value= "name", required =false, defaultValue ="world") String Shirish, Model model){
 
         model.addAttribute("name", Shirish);
         return "greeting";
